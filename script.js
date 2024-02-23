a51e941ca8b25446115ee41c71972f19084f70a0
YuvashreeRchan <popsicle2805@gmail.com>
2024-02-23 17:59:00 +0530

    add media queries

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('Hello!');
+document.addEventListener("DOMContentLoaded", function ()
+{
+    const toggleButton = document.querySelector(".toggle-button");
+    const navList = document.querySelector("#top-nav ul");
+    toggleButton.addEventListener("click", function () 
+    {
+    navList.classList.toggle("active");
+    });
+});
